883e705d1e0c442f091230def88547c48abbe458
sammy17 <chathura.abeyrathne.lk@gmail.com>
2018-07-07 20:41:45 +0530

    Adding sources

---
+++ vivado_tutorial.srcs/sources_1/bd/design_1/hw_handoff/design_1_bd.tcl
@@ -0,0 +1,259 @@
+
+################################################################
+# This is a generated script based on design: design_1
+#
+# Though there are limitations about the generated script,
+# the main purpose of this utility is to make learning
+# IP Integrator Tcl commands easier.
+################################################################
+
+################################################################
+# Check if script is running in correct Vivado version.
+################################################################
+set scripts_vivado_version 2015.4
+set current_vivado_version [version -short]
+
+if { [string first $scripts_vivado_version $current_vivado_version] == -1 } {
+   puts ""
+   puts "ERROR: This script was generated using Vivado <$scripts_vivado_version> and is being run in <$current_vivado_version> of Vivado. Please run the script in Vivado <$scripts_vivado_version> then open the design in Vivado <$current_vivado_version>. Upgrade the design by running \"Tools => Report => Report IP Status...\", then run write_bd_tcl to create an updated script."
+
+   return 1
+}
+
+################################################################
+# START
+################################################################
+
+# To test this script, run the following commands from Vivado Tcl console:
+# source design_1_script.tcl
+
+# If you do not already have a project created,
+# you can create a project using the following command:
+#    create_project project_1 myproj -part xc7z020clg484-1
+#    set_property BOARD_PART xilinx.com:zc702:part0:1.2 [current_project]
+
+# CHECKING IF PROJECT EXISTS
+if { [get_projects -quiet] eq "" } {
+   puts "ERROR: Please open or create a project!"
+   return 1
+}
+
+
+
+# CHANGE DESIGN NAME HERE
+set design_name design_1
+
+# If you do not already have an existing IP Integrator design open,
+# you can create a design using the following command:
+#    create_bd_design $design_name
+
+# Creating design if needed
+set errMsg ""
+set nRet 0
+
+set cur_design [current_bd_design -quiet]
+set list_cells [get_bd_cells -quiet]
+
+if { ${design_name} eq "" } {
+   # USE CASES:
+   #    1) Design_name not set
+
+   set errMsg "ERROR: Please set the variable <design_name> to a non-empty value."
+   set nRet 1
+
+} elseif { ${cur_design} ne "" && ${list_cells} eq "" } {
+   # USE CASES:
+   #    2): Current design opened AND is empty AND names same.
+   #    3): Current design opened AND is empty AND names diff; design_name NOT in project.
+   #    4): Current design opened AND is empty AND names diff; design_name exists in project.
+
+   if { $cur_design ne $design_name } {
+      puts "INFO: Changing value of <design_name> from <$design_name> to <$cur_design> since current design is empty."
+      set design_name [get_property NAME $cur_design]
+   }
+   puts "INFO: Constructing design in IPI design <$cur_design>..."
+
+} elseif { ${cur_design} ne "" && $list_cells ne "" && $cur_design eq $design_name } {
+   # USE CASES:
+   #    5) Current design opened AND has components AND same names.
+
+   set errMsg "ERROR: Design <$design_name> already exists in your project, please set the variable <design_name> to another value."
+   set nRet 1
+} elseif { [get_files -quiet ${design_name}.bd] ne "" } {
+   # USE CASES: 
+   #    6) Current opened design, has components, but diff names, design_name exists in project.
+   #    7) No opened design, design_name exists in project.
+
+   set errMsg "ERROR: Design <$design_name> already exists in your project, please set the variable <design_name> to another value."
+   set nRet 2
+
+} else {
+   # USE CASES:
+   #    8) No opened design, design_name not in project.
+   #    9) Current opened design, has components, but diff names, design_name not in project.
+
+   puts "INFO: Currently there is no design <$design_name> in project, so creating one..."
+
+   create_bd_design $design_name
+
+   puts "INFO: Making design <$design_name> as current_bd_design."
+   current_bd_design $design_name
+
+}
+
+puts "INFO: Currently the variable <design_name> is equal to \"$design_name\"."
+
+if { $nRet != 0 } {
+   puts $errMsg
+   return $nRet
+}
+
+##################################################################
+# DESIGN PROCs
+##################################################################
+
+
+
+# Procedure to create entire design; Provide argument to make
+# procedure reusable. If parentCell is "", will use root.
+proc create_root_design { parentCell } {
+
+  if { $parentCell eq "" } {
+     set parentCell [get_bd_cells /]
+  }
+
+  # Get object for parentCell
+  set parentObj [get_bd_cells $parentCell]
+  if { $parentObj == "" } {
+     puts "ERROR: Unable to find parent cell <$parentCell>!"
+     return
+  }
+
+  # Make sure parentObj is hier blk
+  set parentType [get_property TYPE $parentObj]
+  if { $parentType ne "hier" } {
+     puts "ERROR: Parent <$parentObj> has TYPE = <$parentType>. Expected to be <hier>."
+     return
+  }
+
+  # Save current instance; Restore later
+  set oldCurInst [current_bd_instance .]
+
+  # Set parent object as current
+  current_bd_instance $parentObj
+
+
+  # Create interface ports
+  set DDR [ create_bd_intf_port -mode Master -vlnv xilinx.com:interface:ddrx_rtl:1.0 DDR ]
+  set FIXED_IO [ create_bd_intf_port -mode Master -vlnv xilinx.com:display_processing_system7:fixedio_rtl:1.0 FIXED_IO ]
+
+  # Create ports
+
+  # Create instance: axi_mem_intercon, and set properties
+  set axi_mem_intercon [ create_bd_cell -type ip -vlnv xilinx.com:ip:axi_interconnect:2.1 axi_mem_intercon ]
+  set_property -dict [ list \
+CONFIG.NUM_MI {1} \
+ ] $axi_mem_intercon
+
+  # Create instance: conv_0, and set properties
+  set conv_0 [ create_bd_cell -type ip -vlnv xilinx.com:hls:conv:1.0 conv_0 ]
+
+  # Create instance: processing_system7_0, and set properties
+  set processing_system7_0 [ create_bd_cell -type ip -vlnv xilinx.com:ip:processing_system7:5.5 processing_system7_0 ]
+  set_property -dict [ list \
+CONFIG.PCW_IRQ_F2P_INTR {1} \
+CONFIG.PCW_USE_DEFAULT_ACP_USER_VAL {1} \
+CONFIG.PCW_USE_FABRIC_INTERRUPT {0} \
+CONFIG.PCW_USE_HIGH_OCM {1} \
+CONFIG.PCW_USE_S_AXI_ACP {1} \
+CONFIG.preset {ZC702} \
+ ] $processing_system7_0
+
+  # Create instance: processing_system7_0_axi_periph, and set properties
+  set processing_system7_0_axi_periph [ create_bd_cell -type ip -vlnv xilinx.com:ip:axi_interconnect:2.1 processing_system7_0_axi_periph ]
+  set_property -dict [ list \
+CONFIG.NUM_MI {1} \
+ ] $processing_system7_0_axi_periph
+
+  # Create instance: rst_processing_system7_0_50M, and set properties
+  set rst_processing_system7_0_50M [ create_bd_cell -type ip -vlnv xilinx.com:ip:proc_sys_reset:5.0 rst_processing_system7_0_50M ]
+
+  # Create instance: xlconstant_0, and set properties
+  set xlconstant_0 [ create_bd_cell -type ip -vlnv xilinx.com:ip:xlconstant:1.1 xlconstant_0 ]
+  set_property -dict [ list \
+CONFIG.CONST_VAL {31} \
+CONFIG.CONST_WIDTH {5} \
+ ] $xlconstant_0
+
+  # Create instance: xlconstant_1, and set properties
+  set xlconstant_1 [ create_bd_cell -type ip -vlnv xilinx.com:ip:xlconstant:1.1 xlconstant_1 ]
+  set_property -dict [ list \
+CONFIG.CONST_VAL {15} \
+CONFIG.CONST_WIDTH {4} \
+ ] $xlconstant_1
+
+  # Create interface connections
+  connect_bd_intf_net -intf_net axi_mem_intercon_M00_AXI [get_bd_intf_pins axi_mem_intercon/M00_AXI] [get_bd_intf_pins processing_system7_0/S_AXI_ACP]
+  connect_bd_intf_net -intf_net conv_0_m_axi_CRTL_BUS [get_bd_intf_pins axi_mem_intercon/S00_AXI] [get_bd_intf_pins conv_0/m_axi_CRTL_BUS]
+  connect_bd_intf_net -intf_net processing_system7_0_DDR [get_bd_intf_ports DDR] [get_bd_intf_pins processing_system7_0/DDR]
+  connect_bd_intf_net -intf_net processing_system7_0_FIXED_IO [get_bd_intf_ports FIXED_IO] [get_bd_intf_pins processing_system7_0/FIXED_IO]
+  connect_bd_intf_net -intf_net processing_system7_0_M_AXI_GP0 [get_bd_intf_pins processing_system7_0/M_AXI_GP0] [get_bd_intf_pins processing_system7_0_axi_periph/S00_AXI]
+  connect_bd_intf_net -intf_net processing_system7_0_axi_periph_M00_AXI [get_bd_intf_pins conv_0/s_axi_AXILiteS] [get_bd_intf_pins processing_system7_0_axi_periph/M00_AXI]
+
+  # Create port connections
+  connect_bd_net -net processing_system7_0_FCLK_CLK0 [get_bd_pins axi_mem_intercon/ACLK] [get_bd_pins axi_mem_intercon/M00_ACLK] [get_bd_pins axi_mem_intercon/S00_ACLK] [get_bd_pins conv_0/ap_clk] [get_bd_pins processing_system7_0/FCLK_CLK0] [get_bd_pins processing_system7_0/M_AXI_GP0_ACLK] [get_bd_pins processing_system7_0/S_AXI_ACP_ACLK] [get_bd_pins processing_system7_0_axi_periph/ACLK] [get_bd_pins processing_system7_0_axi_periph/M00_ACLK] [get_bd_pins processing_system7_0_axi_periph/S00_ACLK] [get_bd_pins rst_processing_system7_0_50M/slowest_sync_clk]
+  connect_bd_net -net processing_system7_0_FCLK_RESET0_N [get_bd_pins processing_system7_0/FCLK_RESET0_N] [get_bd_pins rst_processing_system7_0_50M/ext_reset_in]
+  connect_bd_net -net rst_processing_system7_0_50M_interconnect_aresetn [get_bd_pins axi_mem_intercon/ARESETN] [get_bd_pins processing_system7_0_axi_periph/ARESETN] [get_bd_pins rst_processing_system7_0_50M/interconnect_aresetn]
+  connect_bd_net -net rst_processing_system7_0_50M_peripheral_aresetn [get_bd_pins axi_mem_intercon/M00_ARESETN] [get_bd_pins axi_mem_intercon/S00_ARESETN] [get_bd_pins conv_0/ap_rst_n] [get_bd_pins processing_system7_0_axi_periph/M00_ARESETN] [get_bd_pins processing_system7_0_axi_periph/S00_ARESETN] [get_bd_pins rst_processing_system7_0_50M/peripheral_aresetn]
+  connect_bd_net -net xlconstant_0_dout [get_bd_pins processing_system7_0/S_AXI_ACP_ARUSER] [get_bd_pins processing_system7_0/S_AXI_ACP_AWUSER] [get_bd_pins xlconstant_0/dout]
+  connect_bd_net -net xlconstant_1_dout [get_bd_pins processing_system7_0/S_AXI_ACP_ARCACHE] [get_bd_pins processing_system7_0/S_AXI_ACP_AWCACHE] [get_bd_pins xlconstant_1/dout]
+
+  # Create address segments
+  create_bd_addr_seg -range 0x40000000 -offset 0x0 [get_bd_addr_spaces conv_0/Data_m_axi_CRTL_BUS] [get_bd_addr_segs processing_system7_0/S_AXI_ACP/ACP_DDR_LOWOCM] SEG_processing_system7_0_ACP_DDR_LOWOCM
+  create_bd_addr_seg -range 0x40000000 -offset 0x40000000 [get_bd_addr_spaces conv_0/Data_m_axi_CRTL_BUS] [get_bd_addr_segs processing_system7_0/S_AXI_ACP/ACP_M_AXI_GP0] SEG_processing_system7_0_ACP_M_AXI_GP0
+  create_bd_addr_seg -range 0x10000 -offset 0x43C00000 [get_bd_addr_spaces processing_system7_0/Data] [get_bd_addr_segs conv_0/s_axi_AXILiteS/Reg] SEG_conv_0_Reg
+
+  # Perform GUI Layout
+  regenerate_bd_layout -layout_string {
+   guistr: "# # String gsaved with Nlview 6.5.5  2015-06-26 bk=1.3371 VDI=38 GEI=35 GUI=JA:1.6
+#  -string -flagsOSRD
+preplace port DDR -pg 1 -y -220 -defaultsOSRD
+preplace port FIXED_IO -pg 1 -y -200 -defaultsOSRD
+preplace inst xlconstant_0 -pg 1 -lvl 1 -y 460 -defaultsOSRD
+preplace inst xlconstant_1 -pg 1 -lvl 1 -y 230 -defaultsOSRD
+preplace inst rst_processing_system7_0_50M -pg 1 -lvl 3 -y 80 -defaultsOSRD
+preplace inst conv_0 -pg 1 -lvl 1 -y -190 -defaultsOSRD
+preplace inst axi_mem_intercon -pg 1 -lvl 4 -y -380 -defaultsOSRD
+preplace inst processing_system7_0_axi_periph -pg 1 -lvl 4 -y -120 -defaultsOSRD
+preplace inst processing_system7_0 -pg 1 -lvl 2 -y 200 -defaultsOSRD
+preplace netloc processing_system7_0_DDR 1 2 3 NJ -240 NJ -240 NJ
+preplace netloc xlconstant_1_dout 1 1 1 350
+preplace netloc processing_system7_0_axi_periph_M00_AXI 1 0 5 -10 -270 NJ -270 NJ -270 NJ -260 1500
+preplace netloc rst_processing_system7_0_50M_interconnect_aresetn 1 3 1 1170
+preplace netloc processing_system7_0_M_AXI_GP0 1 2 2 NJ -180 N
+preplace netloc processing_system7_0_FCLK_RESET0_N 1 2 1 840
+preplace netloc axi_mem_intercon_M00_AXI 1 1 4 350 -500 NJ -500 NJ -500 1500
+preplace netloc rst_processing_system7_0_50M_peripheral_aresetn 1 0 4 -20 -290 NJ -290 NJ -290 1200
+preplace netloc xlconstant_0_dout 1 1 1 340
+preplace netloc conv_0_m_axi_CRTL_BUS 1 1 3 NJ -440 NJ -440 N
+preplace netloc processing_system7_0_FIXED_IO 1 2 3 NJ -10 NJ 0 NJ
+preplace netloc processing_system7_0_FCLK_CLK0 1 0 4 -10 600 350 670 830 -80 1180
+levelinfo -pg 1 -40 170 580 1000 1350 1800 -top -510 -bot 680
+",
+}
+
+  # Restore current instance
+  current_bd_instance $oldCurInst
+
+  save_bd_design
+}
+# End of create_root_design()
+
+
+##################################################################
+# MAIN FLOW
+##################################################################
+
+create_root_design ""
+
+
---
+++ vivado_tutorial.srcs/sources_1/bd/design_1/ip/design_1_conv_0_2/sim/design_1_conv_0_2.v
@@ -0,0 +1,306 @@
+// (c) Copyright 1995-2018 Xilinx, Inc. All rights reserved.
+// 
+// This file contains confidential and proprietary information
+// of Xilinx, Inc. and is protected under U.S. and
+// international copyright and other intellectual property
+// laws.
+// 
+// DISCLAIMER
+// This disclaimer is not a license and does not grant any
+// rights to the materials distributed herewith. Except as
+// otherwise provided in a valid license issued to you by
+// Xilinx, and to the maximum extent permitted by applicable
+// law: (1) THESE MATERIALS ARE MADE AVAILABLE "AS IS" AND
+// WITH ALL FAULTS, AND XILINX HEREBY DISCLAIMS ALL WARRANTIES
+// AND CONDITIONS, EXPRESS, IMPLIED, OR STATUTORY, INCLUDING
+// BUT NOT LIMITED TO WARRANTIES OF MERCHANTABILITY, NON-
+// INFRINGEMENT, OR FITNESS FOR ANY PARTICULAR PURPOSE; and
+// (2) Xilinx shall not be liable (whether in contract or tort,
+// including negligence, or under any other theory of
+// liability) for any loss or damage of any kind or nature
+// related to, arising under or in connection with these
+// materials, including for any direct, or any indirect,
+// special, incidental, or consequential loss or damage
+// (including loss of data, profits, goodwill, or any type of
+// loss or damage suffered as a result of any action brought
+// by a third party) even if such damage or loss was
+// reasonably foreseeable or Xilinx had been advised of the
+// possibility of the same.
+// 
+// CRITICAL APPLICATIONS
+// Xilinx products are not designed or intended to be fail-
+// safe, or for use in any application requiring fail-safe
+// performance, such as life-support or safety devices or
+// systems, Class III medical devices, nuclear facilities,
+// applications related to the deployment of airbags, or any
+// other applications that could lead to death, personal
+// injury, or severe property or environmental damage
+// (individually and collectively, "Critical
+// Applications"). Customer assumes the sole risk and
+// liability of any use of Xilinx products in Critical
+// Applications, subject only to applicable laws and
+// regulations governing limitations on product liability.
+// 
+// THIS COPYRIGHT NOTICE AND DISCLAIMER MUST BE RETAINED AS
+// PART OF THIS FILE AT ALL TIMES.
+// 
+// DO NOT MODIFY THIS FILE.
+
+
+// IP VLNV: xilinx.com:hls:conv:1.0
+// IP Revision: 1805132348
+
+`timescale 1ns/1ps
+
+(* DowngradeIPIdentifiedWarnings = "yes" *)
+module design_1_conv_0_2 (
+  s_axi_AXILiteS_AWADDR,
+  s_axi_AXILiteS_AWVALID,
+  s_axi_AXILiteS_AWREADY,
+  s_axi_AXILiteS_WDATA,
+  s_axi_AXILiteS_WSTRB,
+  s_axi_AXILiteS_WVALID,
+  s_axi_AXILiteS_WREADY,
+  s_axi_AXILiteS_BRESP,
+  s_axi_AXILiteS_BVALID,
+  s_axi_AXILiteS_BREADY,
+  s_axi_AXILiteS_ARADDR,
+  s_axi_AXILiteS_ARVALID,
+  s_axi_AXILiteS_ARREADY,
+  s_axi_AXILiteS_RDATA,
+  s_axi_AXILiteS_RRESP,
+  s_axi_AXILiteS_RVALID,
+  s_axi_AXILiteS_RREADY,
+  ap_clk,
+  ap_rst_n,
+  interrupt,
+  m_axi_CRTL_BUS_AWADDR,
+  m_axi_CRTL_BUS_AWLEN,
+  m_axi_CRTL_BUS_AWSIZE,
+  m_axi_CRTL_BUS_AWBURST,
+  m_axi_CRTL_BUS_AWLOCK,
+  m_axi_CRTL_BUS_AWREGION,
+  m_axi_CRTL_BUS_AWCACHE,
+  m_axi_CRTL_BUS_AWPROT,
+  m_axi_CRTL_BUS_AWQOS,
+  m_axi_CRTL_BUS_AWVALID,
+  m_axi_CRTL_BUS_AWREADY,
+  m_axi_CRTL_BUS_WDATA,
+  m_axi_CRTL_BUS_WSTRB,
+  m_axi_CRTL_BUS_WLAST,
+  m_axi_CRTL_BUS_WVALID,
+  m_axi_CRTL_BUS_WREADY,
+  m_axi_CRTL_BUS_BRESP,
+  m_axi_CRTL_BUS_BVALID,
+  m_axi_CRTL_BUS_BREADY,
+  m_axi_CRTL_BUS_ARADDR,
+  m_axi_CRTL_BUS_ARLEN,
+  m_axi_CRTL_BUS_ARSIZE,
+  m_axi_CRTL_BUS_ARBURST,
+  m_axi_CRTL_BUS_ARLOCK,
+  m_axi_CRTL_BUS_ARREGION,
+  m_axi_CRTL_BUS_ARCACHE,
+  m_axi_CRTL_BUS_ARPROT,
+  m_axi_CRTL_BUS_ARQOS,
+  m_axi_CRTL_BUS_ARVALID,
+  m_axi_CRTL_BUS_ARREADY,
+  m_axi_CRTL_BUS_RDATA,
+  m_axi_CRTL_BUS_RRESP,
+  m_axi_CRTL_BUS_RLAST,
+  m_axi_CRTL_BUS_RVALID,
+  m_axi_CRTL_BUS_RREADY
+);
+
+(* X_INTERFACE_INFO = "xilinx.com:interface:aximm:1.0 s_axi_AXILiteS AWADDR" *)
+input wire [5 : 0] s_axi_AXILiteS_AWADDR;
+(* X_INTERFACE_INFO = "xilinx.com:interface:aximm:1.0 s_axi_AXILiteS AWVALID" *)
+input wire s_axi_AXILiteS_AWVALID;
+(* X_INTERFACE_INFO = "xilinx.com:interface:aximm:1.0 s_axi_AXILiteS AWREADY" *)
+output wire s_axi_AXILiteS_AWREADY;
+(* X_INTERFACE_INFO = "xilinx.com:interface:aximm:1.0 s_axi_AXILiteS WDATA" *)
+input wire [31 : 0] s_axi_AXILiteS_WDATA;
+(* X_INTERFACE_INFO = "xilinx.com:interface:aximm:1.0 s_axi_AXILiteS WSTRB" *)
+input wire [3 : 0] s_axi_AXILiteS_WSTRB;
+(* X_INTERFACE_INFO = "xilinx.com:interface:aximm:1.0 s_axi_AXILiteS WVALID" *)
+input wire s_axi_AXILiteS_WVALID;
+(* X_INTERFACE_INFO = "xilinx.com:interface:aximm:1.0 s_axi_AXILiteS WREADY" *)
+output wire s_axi_AXILiteS_WREADY;
+(* X_INTERFACE_INFO = "xilinx.com:interface:aximm:1.0 s_axi_AXILiteS BRESP" *)
+output wire [1 : 0] s_axi_AXILiteS_BRESP;
+(* X_INTERFACE_INFO = "xilinx.com:interface:aximm:1.0 s_axi_AXILiteS BVALID" *)
+output wire s_axi_AXILiteS_BVALID;
+(* X_INTERFACE_INFO = "xilinx.com:interface:aximm:1.0 s_axi_AXILiteS BREADY" *)
+input wire s_axi_AXILiteS_BREADY;
+(* X_INTERFACE_INFO = "xilinx.com:interface:aximm:1.0 s_axi_AXILiteS ARADDR" *)
+input wire [5 : 0] s_axi_AXILiteS_ARADDR;
+(* X_INTERFACE_INFO = "xilinx.com:interface:aximm:1.0 s_axi_AXILiteS ARVALID" *)
+input wire s_axi_AXILiteS_ARVALID;
+(* X_INTERFACE_INFO = "xilinx.com:interface:aximm:1.0 s_axi_AXILiteS ARREADY" *)
+output wire s_axi_AXILiteS_ARREADY;
+(* X_INTERFACE_INFO = "xilinx.com:interface:aximm:1.0 s_axi_AXILiteS RDATA" *)
+output wire [31 : 0] s_axi_AXILiteS_RDATA;
+(* X_INTERFACE_INFO = "xilinx.com:interface:aximm:1.0 s_axi_AXILiteS RRESP" *)
+output wire [1 : 0] s_axi_AXILiteS_RRESP;
+(* X_INTERFACE_INFO = "xilinx.com:interface:aximm:1.0 s_axi_AXILiteS RVALID" *)
+output wire s_axi_AXILiteS_RVALID;
+(* X_INTERFACE_INFO = "xilinx.com:interface:aximm:1.0 s_axi_AXILiteS RREADY" *)
+input wire s_axi_AXILiteS_RREADY;
+(* X_INTERFACE_INFO = "xilinx.com:signal:clock:1.0 ap_clk CLK" *)
+input wire ap_clk;
+(* X_INTERFACE_INFO = "xilinx.com:signal:reset:1.0 ap_rst_n RST" *)
+input wire ap_rst_n;
+(* X_INTERFACE_INFO = "xilinx.com:signal:interrupt:1.0 interrupt INTERRUPT" *)
+output wire interrupt;
+(* X_INTERFACE_INFO = "xilinx.com:interface:aximm:1.0 m_axi_CRTL_BUS AWADDR" *)
+output wire [31 : 0] m_axi_CRTL_BUS_AWADDR;
+(* X_INTERFACE_INFO = "xilinx.com:interface:aximm:1.0 m_axi_CRTL_BUS AWLEN" *)
+output wire [7 : 0] m_axi_CRTL_BUS_AWLEN;
+(* X_INTERFACE_INFO = "xilinx.com:interface:aximm:1.0 m_axi_CRTL_BUS AWSIZE" *)
+output wire [2 : 0] m_axi_CRTL_BUS_AWSIZE;
+(* X_INTERFACE_INFO = "xilinx.com:interface:aximm:1.0 m_axi_CRTL_BUS AWBURST" *)
+output wire [1 : 0] m_axi_CRTL_BUS_AWBURST;
+(* X_INTERFACE_INFO = "xilinx.com:interface:aximm:1.0 m_axi_CRTL_BUS AWLOCK" *)
+output wire [1 : 0] m_axi_CRTL_BUS_AWLOCK;
+(* X_INTERFACE_INFO = "xilinx.com:interface:aximm:1.0 m_axi_CRTL_BUS AWREGION" *)
+output wire [3 : 0] m_axi_CRTL_BUS_AWREGION;
+(* X_INTERFACE_INFO = "xilinx.com:interface:aximm:1.0 m_axi_CRTL_BUS AWCACHE" *)
+output wire [3 : 0] m_axi_CRTL_BUS_AWCACHE;
+(* X_INTERFACE_INFO = "xilinx.com:interface:aximm:1.0 m_axi_CRTL_BUS AWPROT" *)
+output wire [2 : 0] m_axi_CRTL_BUS_AWPROT;
+(* X_INTERFACE_INFO = "xilinx.com:interface:aximm:1.0 m_axi_CRTL_BUS AWQOS" *)
+output wire [3 : 0] m_axi_CRTL_BUS_AWQOS;
+(* X_INTERFACE_INFO = "xilinx.com:interface:aximm:1.0 m_axi_CRTL_BUS AWVALID" *)
+output wire m_axi_CRTL_BUS_AWVALID;
+(* X_INTERFACE_INFO = "xilinx.com:interface:aximm:1.0 m_axi_CRTL_BUS AWREADY" *)
+input wire m_axi_CRTL_BUS_AWREADY;
+(* X_INTERFACE_INFO = "xilinx.com:interface:aximm:1.0 m_axi_CRTL_BUS WDATA" *)
+output wire [31 : 0] m_axi_CRTL_BUS_WDATA;
+(* X_INTERFACE_INFO = "xilinx.com:interface:aximm:1.0 m_axi_CRTL_BUS WSTRB" *)
+output wire [3 : 0] m_axi_CRTL_BUS_WSTRB;
+(* X_INTERFACE_INFO = "xilinx.com:interface:aximm:1.0 m_axi_CRTL_BUS WLAST" *)
+output wire m_axi_CRTL_BUS_WLAST;
+(* X_INTERFACE_INFO = "xilinx.com:interface:aximm:1.0 m_axi_CRTL_BUS WVALID" *)
+output wire m_axi_CRTL_BUS_WVALID;
+(* X_INTERFACE_INFO = "xilinx.com:interface:aximm:1.0 m_axi_CRTL_BUS WREADY" *)
+input wire m_axi_CRTL_BUS_WREADY;
+(* X_INTERFACE_INFO = "xilinx.com:interface:aximm:1.0 m_axi_CRTL_BUS BRESP" *)
+input wire [1 : 0] m_axi_CRTL_BUS_BRESP;
+(* X_INTERFACE_INFO = "xilinx.com:interface:aximm:1.0 m_axi_CRTL_BUS BVALID" *)
+input wire m_axi_CRTL_BUS_BVALID;
+(* X_INTERFACE_INFO = "xilinx.com:interface:aximm:1.0 m_axi_CRTL_BUS BREADY" *)
+output wire m_axi_CRTL_BUS_BREADY;
+(* X_INTERFACE_INFO = "xilinx.com:interface:aximm:1.0 m_axi_CRTL_BUS ARADDR" *)
+output wire [31 : 0] m_axi_CRTL_BUS_ARADDR;
+(* X_INTERFACE_INFO = "xilinx.com:interface:aximm:1.0 m_axi_CRTL_BUS ARLEN" *)
+output wire [7 : 0] m_axi_CRTL_BUS_ARLEN;
+(* X_INTERFACE_INFO = "xilinx.com:interface:aximm:1.0 m_axi_CRTL_BUS ARSIZE" *)
+output wire [2 : 0] m_axi_CRTL_BUS_ARSIZE;
+(* X_INTERFACE_INFO = "xilinx.com:interface:aximm:1.0 m_axi_CRTL_BUS ARBURST" *)
+output wire [1 : 0] m_axi_CRTL_BUS_ARBURST;
+(* X_INTERFACE_INFO = "xilinx.com:interface:aximm:1.0 m_axi_CRTL_BUS ARLOCK" *)
+output wire [1 : 0] m_axi_CRTL_BUS_ARLOCK;
+(* X_INTERFACE_INFO = "xilinx.com:interface:aximm:1.0 m_axi_CRTL_BUS ARREGION" *)
+output wire [3 : 0] m_axi_CRTL_BUS_ARREGION;
+(* X_INTERFACE_INFO = "xilinx.com:interface:aximm:1.0 m_axi_CRTL_BUS ARCACHE" *)
+output wire [3 : 0] m_axi_CRTL_BUS_ARCACHE;
+(* X_INTERFACE_INFO = "xilinx.com:interface:aximm:1.0 m_axi_CRTL_BUS ARPROT" *)
+output wire [2 : 0] m_axi_CRTL_BUS_ARPROT;
+(* X_INTERFACE_INFO = "xilinx.com:interface:aximm:1.0 m_axi_CRTL_BUS ARQOS" *)
+output wire [3 : 0] m_axi_CRTL_BUS_ARQOS;
+(* X_INTERFACE_INFO = "xilinx.com:interface:aximm:1.0 m_axi_CRTL_BUS ARVALID" *)
+output wire m_axi_CRTL_BUS_ARVALID;
+(* X_INTERFACE_INFO = "xilinx.com:interface:aximm:1.0 m_axi_CRTL_BUS ARREADY" *)
+input wire m_axi_CRTL_BUS_ARREADY;
+(* X_INTERFACE_INFO = "xilinx.com:interface:aximm:1.0 m_axi_CRTL_BUS RDATA" *)
+input wire [31 : 0] m_axi_CRTL_BUS_RDATA;
+(* X_INTERFACE_INFO = "xilinx.com:interface:aximm:1.0 m_axi_CRTL_BUS RRESP" *)
+input wire [1 : 0] m_axi_CRTL_BUS_RRESP;
+(* X_INTERFACE_INFO = "xilinx.com:interface:aximm:1.0 m_axi_CRTL_BUS RLAST" *)
+input wire m_axi_CRTL_BUS_RLAST;
+(* X_INTERFACE_INFO = "xilinx.com:interface:aximm:1.0 m_axi_CRTL_BUS RVALID" *)
+input wire m_axi_CRTL_BUS_RVALID;
+(* X_INTERFACE_INFO = "xilinx.com:interface:aximm:1.0 m_axi_CRTL_BUS RREADY" *)
+output wire m_axi_CRTL_BUS_RREADY;
+
+  conv #(
+    .C_S_AXI_AXILITES_ADDR_WIDTH(6),
+    .C_S_AXI_AXILITES_DATA_WIDTH(32),
+    .C_M_AXI_CRTL_BUS_ID_WIDTH(1),
+    .C_M_AXI_CRTL_BUS_ADDR_WIDTH(32),
+    .C_M_AXI_CRTL_BUS_DATA_WIDTH(32),
+    .C_M_AXI_CRTL_BUS_AWUSER_WIDTH(1),
+    .C_M_AXI_CRTL_BUS_ARUSER_WIDTH(1),
+    .C_M_AXI_CRTL_BUS_WUSER_WIDTH(1),
+    .C_M_AXI_CRTL_BUS_RUSER_WIDTH(1),
+    .C_M_AXI_CRTL_BUS_BUSER_WIDTH(1),
+    .C_M_AXI_CRTL_BUS_USER_VALUE('H00000000),
+    .C_M_AXI_CRTL_BUS_PROT_VALUE('B000),
+    .C_M_AXI_CRTL_BUS_CACHE_VALUE('B0011)
+  ) inst (
+    .s_axi_AXILiteS_AWADDR(s_axi_AXILiteS_AWADDR),
+    .s_axi_AXILiteS_AWVALID(s_axi_AXILiteS_AWVALID),
+    .s_axi_AXILiteS_AWREADY(s_axi_AXILiteS_AWREADY),
+    .s_axi_AXILiteS_WDATA(s_axi_AXILiteS_WDATA),
+    .s_axi_AXILiteS_WSTRB(s_axi_AXILiteS_WSTRB),
+    .s_axi_AXILiteS_WVALID(s_axi_AXILiteS_WVALID),
+    .s_axi_AXILiteS_WREADY(s_axi_AXILiteS_WREADY),
+    .s_axi_AXILiteS_BRESP(s_axi_AXILiteS_BRESP),
+    .s_axi_AXILiteS_BVALID(s_axi_AXILiteS_BVALID),
+    .s_axi_AXILiteS_BREADY(s_axi_AXILiteS_BREADY),
+    .s_axi_AXILiteS_ARADDR(s_axi_AXILiteS_ARADDR),
+    .s_axi_AXILiteS_ARVALID(s_axi_AXILiteS_ARVALID),
+    .s_axi_AXILiteS_ARREADY(s_axi_AXILiteS_ARREADY),
+    .s_axi_AXILiteS_RDATA(s_axi_AXILiteS_RDATA),
+    .s_axi_AXILiteS_RRESP(s_axi_AXILiteS_RRESP),
+    .s_axi_AXILiteS_RVALID(s_axi_AXILiteS_RVALID),
+    .s_axi_AXILiteS_RREADY(s_axi_AXILiteS_RREADY),
+    .ap_clk(ap_clk),
+    .ap_rst_n(ap_rst_n),
+    .interrupt(interrupt),
+    .m_axi_CRTL_BUS_AWID(),
+    .m_axi_CRTL_BUS_AWADDR(m_axi_CRTL_BUS_AWADDR),
+    .m_axi_CRTL_BUS_AWLEN(m_axi_CRTL_BUS_AWLEN),
+    .m_axi_CRTL_BUS_AWSIZE(m_axi_CRTL_BUS_AWSIZE),
+    .m_axi_CRTL_BUS_AWBURST(m_axi_CRTL_BUS_AWBURST),
+    .m_axi_CRTL_BUS_AWLOCK(m_axi_CRTL_BUS_AWLOCK),
+    .m_axi_CRTL_BUS_AWREGION(m_axi_CRTL_BUS_AWREGION),
+    .m_axi_CRTL_BUS_AWCACHE(m_axi_CRTL_BUS_AWCACHE),
+    .m_axi_CRTL_BUS_AWPROT(m_axi_CRTL_BUS_AWPROT),
+    .m_axi_CRTL_BUS_AWQOS(m_axi_CRTL_BUS_AWQOS),
+    .m_axi_CRTL_BUS_AWUSER(),
+    .m_axi_CRTL_BUS_AWVALID(m_axi_CRTL_BUS_AWVALID),
+    .m_axi_CRTL_BUS_AWREADY(m_axi_CRTL_BUS_AWREADY),
+    .m_axi_CRTL_BUS_WID(),
+    .m_axi_CRTL_BUS_WDATA(m_axi_CRTL_BUS_WDATA),
+    .m_axi_CRTL_BUS_WSTRB(m_axi_CRTL_BUS_WSTRB),
+    .m_axi_CRTL_BUS_WLAST(m_axi_CRTL_BUS_WLAST),
+    .m_axi_CRTL_BUS_WUSER(),
+    .m_axi_CRTL_BUS_WVALID(m_axi_CRTL_BUS_WVALID),
+    .m_axi_CRTL_BUS_WREADY(m_axi_CRTL_BUS_WREADY),
+    .m_axi_CRTL_BUS_BID(1'B0),
+    .m_axi_CRTL_BUS_BRESP(m_axi_CRTL_BUS_BRESP),
+    .m_axi_CRTL_BUS_BUSER(1'B0),
+    .m_axi_CRTL_BUS_BVALID(m_axi_CRTL_BUS_BVALID),
+    .m_axi_CRTL_BUS_BREADY(m_axi_CRTL_BUS_BREADY),
+    .m_axi_CRTL_BUS_ARID(),
+    .m_axi_CRTL_BUS_ARADDR(m_axi_CRTL_BUS_ARADDR),
+    .m_axi_CRTL_BUS_ARLEN(m_axi_CRTL_BUS_ARLEN),
+    .m_axi_CRTL_BUS_ARSIZE(m_axi_CRTL_BUS_ARSIZE),
+    .m_axi_CRTL_BUS_ARBURST(m_axi_CRTL_BUS_ARBURST),
+    .m_axi_CRTL_BUS_ARLOCK(m_axi_CRTL_BUS_ARLOCK),
+    .m_axi_CRTL_BUS_ARREGION(m_axi_CRTL_BUS_ARREGION),
+    .m_axi_CRTL_BUS_ARCACHE(m_axi_CRTL_BUS_ARCACHE),
+    .m_axi_CRTL_BUS_ARPROT(m_axi_CRTL_BUS_ARPROT),
+    .m_axi_CRTL_BUS_ARQOS(m_axi_CRTL_BUS_ARQOS),
+    .m_axi_CRTL_BUS_ARUSER(),
+    .m_axi_CRTL_BUS_ARVALID(m_axi_CRTL_BUS_ARVALID),
+    .m_axi_CRTL_BUS_ARREADY(m_axi_CRTL_BUS_ARREADY),
+    .m_axi_CRTL_BUS_RID(1'B0),
+    .m_axi_CRTL_BUS_RDATA(m_axi_CRTL_BUS_RDATA),
+    .m_axi_CRTL_BUS_RRESP(m_axi_CRTL_BUS_RRESP),
+    .m_axi_CRTL_BUS_RLAST(m_axi_CRTL_BUS_RLAST),
+    .m_axi_CRTL_BUS_RUSER(1'B0),
+    .m_axi_CRTL_BUS_RVALID(m_axi_CRTL_BUS_RVALID),
+    .m_axi_CRTL_BUS_RREADY(m_axi_CRTL_BUS_RREADY)
+  );
+endmodule
